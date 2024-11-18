8949bb3226c9bff230c57ba9fe04fa4ef286ce65
Ons Ben Amor <60884713+ons33@users.noreply.github.com>
2024-11-18 12:50:10 +0100

    seance2

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+VAR1= 10;
+VAR2="https://seeklogo.com/images/A/angular-icon-logo-9946B9795D-seeklogo.com.png"
+VAR3="Mahdi"
+list1=["Mahdi","Ali","Hassan","Hossein"]
+Function1(){
+  alert("Hello 4SE3");
+}
+
+}
